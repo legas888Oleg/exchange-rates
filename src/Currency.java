a657f3f546a75c6e0b3ba6882f04408ba9a9c5f2
legas <legas888@yandex.ru>
2022-06-30 13:21:29 +0300

    I made the output of exchange rates on the JFrame

--- src/Currency.java
+++ src/Currency.java
@@ -3,8 +3,12 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
@@ -15,6 +19,28 @@ import java.util.Map;
 public class Currency {
     public static void main(String[] args) throws Exception {
         String[][] rates = getRates();
+        JFrame frame = new JFrame("Курс валют");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String[] columnNames = {"Код валюты", "Цена"};
+        JTable table = new JTable(rates, columnNames);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        JTableHeader header = table.getTableHeader();
+        header.setFont(new Font("Arial", Font.BOLD, 20));
+        table.setFont(new Font("Serif", Font.PLAIN, 18));
+        table.setRowHeight(table.getRowHeight() + 18);
+
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
+        table.getColumnModel().getColumn(0).setCellRenderer(renderer);
+        table.getColumnModel().getColumn(1).setCellRenderer(renderer);
+
+        frame.add(scrollPane);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
         for (String[] rate : rates) {
             System.out.println(rate[0] + " " + rate[1]);
@@ -58,7 +84,7 @@ public class Currency {
                 }
             }
             rates[k][0] = entry.getKey();
-            rates[k][1] = ((double)Math.round(value / nominal * 10000)) / 10000 + " рублей";
+            rates[k][1] = ((double)Math.round(value / nominal * 10000)) / 10000 + " руб.";
             k++;
         }
         return rates;
